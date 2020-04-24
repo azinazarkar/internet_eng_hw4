6a48071f9f90a7594db3a456ad2ebf25c61ecacb
unknown <azin.azarkar@gmail.com>
2020-04-24 22:23:16 +0430

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname ));
+var fs = require('fs');
+const axios = require("axios");
+
+var deleteProduct = function(element){
+    console.log(element);
+}
+app.get("/", function(req, res){
+    var locations ; 
+    axios.get('http://covid19api.xapix.io/v2/locations').then((response) => {
+        locations = response.data.locations;
+        res.render('index', {locations: locations ,
+                             clickHandler: ()=>{console.log("inja")}, 
+                             deleteProduct : deleteProduct})
+    }).catch((error) =>  {return "ERROR";} );
+});
+
+app.listen(port, () => {
+    console.log(`Listening to requests on http://localhost:${port}`);
+});
